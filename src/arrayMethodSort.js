81e3a9ae9149e4b1296db46d228894f65b738805
Vladyslava Korovaina <vladyslava.korovainaya@gmail.com>
2024-12-14 13:02:07 +0000

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,22 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    // eslint-disable-next-line no-param-reassign
+    compareFunction =
+      typeof compareFunction === 'function'
+        ? compareFunction
+        : (a, b) => String(a).localeCompare(String(b));
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
